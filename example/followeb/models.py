45e4daa02f3668b4846483412b9e8b9687a72f54
Jet <jet@localhost.localdomain>
2013-03-22 17:04:57 -0500

    first model version

--- example/followeb/models.py
+++ example/followeb/models.py
@@ -2,4 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class Resource(models.Model):
+	
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=300, blank=True)
+	url = models.URLField(editable=False)
+	
+	def __unicode__(self):
+		return self.title
 
+class ResourceVersion(models.Model):
+	
+	resource = models.ForeignKey(Resource)
+	version = models.IntegerField(editable=False)
+	date = models.DateTimeField(blank=True, editable=False)
+	resource_file = models.FileField(upload_to='./file_history/', editable=False)
+	
+	def __unicode__(self):
+		return self.resource.title+'_'+str(self.version) 
